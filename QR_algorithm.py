169c15ed353632a5d981988001b684b54005ac71
xd1002 <66120916+xd1002@users.noreply.github.com>
2021-08-12 09:33:37 +0800

    Add files via upload

--- QR_algorithm.py
+++ QR_algorithm.py
@@ -8,7 +8,7 @@ Created on Mon Aug  9 10:01:57 2021
 
 import numpy as np
 import copy
-np.set_printoptions(precision=8)
+
 class QR_algorithm():
     def __init__(self,x):
         self.x=x
@@ -68,13 +68,18 @@ class QR_algorithm():
                     
         return Q,R
     
-    
-    def eigvalue(self,x,max_iter_time):
+
+
+
+
+
+    #epsilon决定了在数据的绝对值小于多少时认为他为0（类似与计算精度？）个人感觉10的-10次方好一点，这个值太大了太小了都会影响精度，尤其是太小了（如10的-20次方），会出现严重的计算错误。但10的-5次方等的情况下会出现精度不足的情况
+    #建议尝试多个精度后取平均
+    def eigvalue(self,x,max_iter_time,epsilon):
         x=self.upper_hessenberg(x)
         
         n=x.shape[0]-1
         s=x[n,n]
-        epsilon=10**(-10)
         iter_time=0
         self.eig_value=[]
         while n>=1:
@@ -108,6 +113,7 @@ class QR_algorithm():
                 iter_time=0
                 n-=1
                 x=x[:n+1,:n+1]
+                s=x[n,n]
                 
         
                 
@@ -121,7 +127,7 @@ class QR_algorithm():
             
         for i in range(location,x.shape[0]):
             for j in range(location,x.shape[1]):
-                if x[i,j]!=0:
+                if x[i,j]>=10**(-5):
                         
                     t=copy.deepcopy(x[location,:])
                     x[location,:]=x[i,:]
@@ -134,7 +140,7 @@ class QR_algorithm():
                 
         #通过初等行变换将矩阵化为上三角矩阵
         for i in range(x.shape[0]-1):
-            if (x[i+1:,i+1:]==0).all()==False:    
+            if (x[i+1:,i+1:]<=10**(-5)).all()==False:    
                 self.change_element(x,i)
             else:
                 break
@@ -152,41 +158,48 @@ class QR_algorithm():
     
     #求出基础解系，并默认各基向量间以系数1相加输出一个解        
     def solve(self,x):
-            A=copy.deepcopy(x)
-            A=np.asarray(A,dtype=complex)
-            if np.abs(self.calculate_determinant(A))>=10**(-10):
-                print('end end end')
-                return np.zeros(A.shape[0])
-            
-            else:
+                A=copy.deepcopy(x)
+                A=np.asarray(A,dtype=np.complex)
+                A_average=np.sum(A,axis=1)
+                A_average=np.sum(A_average)
+                A=A/np.complex(A_average)
+                
+            #if np.abs(self.calculate_determinant(A))>=10**(-5):
+             #   print('end end end')
+              #  return np.zeros(A.shape[0])
+            #
+            #else:
                 
                 #上面if的判断中已经算过了不用再算一边了
+                for i in range(A.shape[0]-1):
+                    if (np.abs(A[i+1:,i+1:])<=10**(-5)).all()==False:    
+                        self.change_element(A,i)
+                    else:
+                        break
+                    for j in range(i+1,A.shape[0]):
+                        A[j,:]=A[j,:]-A[i,:]*A[j,i]/A[i,i]
                 
-                #for i in range(A.shape[0]-1):
-                    #if (A[i+1:,i+1:]==0).all()==False:    
-                        #self.change_element(A,i)
-                    #else:
-                        #break
-                    #for j in range(i+1,A.shape[0]):
-                        #A[j,:]=A[j,:]-A[i,:]*A[j,i]/A[i,i]
                         
+                
                 for i in range(1,A.shape[0]):
-                    if (np.abs(A[i,:])<=10**(-10)).all()==False:
+                    if (np.abs(A[i,:])<=10**(-5)).all()==False:
                         for k in range(i,A.shape[0]):
-                            if np.abs(A[i,k])>=10*(-10):
+                            if np.abs(A[i,k])>=10*(-5):
                                 
                                 for j in range(i):
                                     A[j,:]-=A[j,k]*A[i,:]/A[i,k]
+                                
                                 break
                             
                     else:
                         break
                 
                 index=[]
+            
                 for i in range(A.shape[0]):
-                    if (np.abs(A[i,:])<=10**(-10)).all()==False:
+                    if (np.abs(A[i,:])<=10**(-5)).all()==False:
                         for j in range(i,A.shape[0]):
-                            if np.abs(A[i,j])>=10**(-10):
+                            if np.abs(A[i,j])>=10**(-5):
                                 A[i,:]=A[i,:]/A[i,j]
                                 index.append(j)
                                 break
@@ -203,28 +216,19 @@ class QR_algorithm():
                 solution=-np.delete(copy.deepcopy(A),index,1)
                 self.basic_solution=np.delete(np.eye(A.shape[0],dtype=np.complex),index,1)
                 self.basic_solution+=solution
+                self.basic_solution=self.basic_solution.reshape(A.shape[0],-1)
                 
-                return np.sum(self.basic_solution,axis=1)
+                #return np.sum(self.basic_solution,axis=1)
             
             
     def calculate_eig_vector(self,x,eig_value):
         self.eig_vector=np.zeros((x.shape[0],len(eig_value)),dtype=np.complex)
         for i in range(len(eig_value)):
-            self.eig_vector[:,i]=self.solve(x-eig_value[i]*np.eye(x.shape[0]))
+            self.solve(x-eig_value[i]*np.eye(x.shape[0]))
+            if i!=0 and np.abs(eig_value[i]-eig_value[i-1])<=10**(-10):
+                self.eig_vector[:,i]=(i+1)*self.basic_solution[:,0]+(i+2)*self.basic_solution[:,0]
+            else:
+                self.eig_vector[:,i]=(i+1)*self.basic_solution[:,0]
+        return self.eig_vector
 
     
-            
-
-xx=np.array([[1,2,3,5],[4,5,6,10],[7,8,0,8],[7,3,11,0]])
-np.linalg.eig(xx)
-model=QR_algorithm(xx)
-model.eigvalue(xx,50)
-model.calculate_eig_vector(xx,model.eig_value)
-model.eig_vector
-model.solve(xx-model.eig_value[3]*np.eye(4))
-model.basic_solution
-np.abs(model.calculate_determinant(xx-model.eig_value[3]*np.eye(4)))>=10**(-10)
-model.basic_solution
-model.calculate_determinant(xx-model.eig_value[3]*np.eye(xx.shape[0]))
-np.matmul(xx,model.eig_vector[:,0])-model.eig_value[0]*model.eig_vector[:,0]
-
